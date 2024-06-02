6c44fdde7873a9a5d9f06bbadd5405cb0fa0a49b
szialajoscosplay <70654182+k3rielit@users.noreply.github.com>
2024-06-02 17:53:28 +0200

    ModashApi class skeleton

---
+++ Scrapedash/ModashClient/API/ModashApi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModashClient.API {
+
+    public class ModashApi : IDisposable {
+
+        public ApiClient Api {  get; private set; }
+
+        public ModashApi() {
+            Api = new ApiClient();
+        }
+
+        public void Dispose() {
+            Api.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+    }
+
+}
