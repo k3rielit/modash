73e5f4e13b28cf75781db59b164379c1b1353a61
szialajoscosplay <70654182+k3rielit@users.noreply.github.com>
2024-06-02 17:56:47 +0200

    Cleaned up ModashApi

--- Scrapedash/ModashClient/API/ModashApi.cs
+++ Scrapedash/ModashClient/API/ModashApi.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ModashClient.API {
+﻿namespace ModashClient.API {
 
     public class ModashApi : IDisposable {
 
-        public ApiClient Api {  get; private set; }
+        public ApiClient Api { get; private set; }
 
         public ModashApi() {
             Api = new ApiClient();
