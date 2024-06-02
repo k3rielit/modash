9c66ff593170d43a7abfb21a04694a9d741a6427
szialajoscosplay <70654182+k3rielit@users.noreply.github.com>
2024-06-02 17:21:01 +0200

    ModashAccount config, JsonSettings default configuration

---
+++ Scrapedash/ModashClient/Configuration/JsonSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ModashClient.Configuration
+{
+
+    public class JsonSettings
+    {
+
+        public static readonly JsonSerializerSettings DefaultSettings = new JsonSerializerSettings {
+            NullValueHandling = NullValueHandling.Ignore,
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Formatting = Formatting.Indented,
+            Converters = {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+
+    }
+
+}
---
+++ Scrapedash/ModashClient/Configuration/ModashAccount.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModashClient.Configuration {
+
+    public class ModashAccount {
+
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Cookies { get; set; } = string.Empty;
+
+        public static bool ConfigurationExists() {
+            return File.Exists("account.json");
+        }
+
+        public static bool ConfigurationWritable() {
+            if(!ConfigurationExists()) {
+                return false;
+            }
+            try {
+                using var stream = File.Open("account.json", FileMode.Open, FileAccess.Read);
+                return true;
+            }
+            catch(Exception) {
+                return false;
+            }
+        }
+
+        public static bool ConfigurationReadable() {
+            if(!ConfigurationExists()) {
+                return false;
+            }
+            try {
+                using var stream = File.Open("account.json", FileMode.Open, FileAccess.Write);
+                return true;
+            }
+            catch(Exception) {
+                return false;
+            }
+        }
+
+        public static ModashAccount Load() {
+            var defaultConfig = new ModashAccount();
+            if(ConfigurationExists() && ConfigurationWritable()) {
+                return JsonConvert.DeserializeObject<ModashAccount>(File.ReadAllText("account.json")) ?? defaultConfig;
+            }
+            return defaultConfig;
+        }
+
+        public void Save() {
+            if(!ConfigurationExists() || ConfigurationWritable()) {
+                File.WriteAllText("account.json", JsonConvert.SerializeObject(this, JsonSettings.DefaultSettings));
+            }
+        }
+
+    }
+
+}
