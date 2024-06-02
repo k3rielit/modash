ea5c23a39b8928baaf33c7e181c02faded99ef50
szialajoscosplay <70654182+k3rielit@users.noreply.github.com>
2024-06-02 17:43:43 +0200

    Added Modash ApiClient

---
+++ Scrapedash/ModashClient/API/ApiClient.cs
@@ -0,0 +1,44 @@
+﻿using ModashClient.Configuration;
+using Newtonsoft.Json;
+using System;
+
+namespace ModashClient.API {
+
+    public class ApiClient {
+
+        public HttpClient Client { get; private set; }
+
+        public ApiClient() {
+            Client = new HttpClient();
+        }
+
+        public async Task<T?> Request<T>(HttpMethod method, string uri, string cookies, string? body = null) {
+            T? result = default;
+            var request = body == null ? new HttpRequestMessage {
+                Method = method,
+                RequestUri = new Uri(uri),
+                Headers = {
+                    { "Cookies", cookies },
+                },
+            } : new HttpRequestMessage {
+                Method = method,
+                RequestUri = new Uri(uri),
+                Headers = {
+                    { "Cookies", cookies },
+                },
+                Content = new StringContent(body),
+            };
+            try {
+                using var response = await Client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                var str = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(str, JsonSettings.DefaultSettings) ?? result;
+            }
+            catch(Exception) {
+                return result;
+            }
+        }
+
+    }
+
+}
